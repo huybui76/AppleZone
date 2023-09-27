34696e78a9bc23cfae199022bdd738af979d6079
Diệp Phi <127822124+Jellyvous@users.noreply.github.com>
2023-09-27 16:07:52 +0700

    Diep Phi

--- src/components/pages/Homepage.jsx
+++ src/components/pages/Homepage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-
+import './Homepage.css'
 import search_icon from '../assets/search-interface-symbol.png'
 import shopping_icon from '../assets/shopping-cart.png'
 import user_icon from '../assets/user.png'
@@ -17,8 +17,8 @@ const Homepage = () => {
         <div className="nav-menu">Âm thanh</div>
         <div className="nav-menu">Phụ kiện</div>
         <input type="text" className="productsInput" placeholder='Tìm kiếm sản phẩm'/>
-        <img src={shopping_icon} alt="" style={{width:50, height:50,}}/>
-        <img src={user_icon} alt="" style={{width:200, height:200,}}/>
+        <img src={shopping_icon} alt="" style={{width:40, height:40,}}/>
+        <img src={user_icon} alt="" style={{width:40, height:40,}}/>
       </div>
     </div>
   )
