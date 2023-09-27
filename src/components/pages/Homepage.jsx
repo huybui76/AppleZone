9fc5519285041e44be3121a92143536df82f3f5e
Diệp Phi <127822124+Jellyvous@users.noreply.github.com>
2023-09-27 18:55:08 +0700

    Phi-navbar

--- src/components/pages/Homepage.jsx
+++ src/components/pages/Homepage.jsx
@@ -7,19 +7,29 @@ import user_icon from '../assets/user.png'
 
 const Homepage = () => {
   return (
-    <div className="header">
-      <div className="navbar">
-        <div className="nav-pagename">Apple Zone</div>
-        <div className="nav-menu">Iphone</div>
-        <div className="nav-menu">Mac</div>
-        <div className="nav-menu">Ipad</div>
-        <div className="nav-menu">Watch</div>
-        <div className="nav-menu">Âm thanh</div>
-        <div className="nav-menu">Phụ kiện</div>
-        <input type="text" className="productsInput" placeholder='Tìm kiếm sản phẩm'/>
-        <img src={shopping_icon} alt="" style={{width:40, height:40,}}/>
-        <img src={user_icon} alt="" style={{width:40, height:40,}}/>
-      </div>
+    <div className="container">
+      <div className="header">
+        <div className="navbar">
+          <div className="nav-pagename">Apple Zone</div>
+          <div className="nav-menu">
+            <div className="menu-component">Iphone</div>
+            <div className="menu-component">Mac</div>
+            <div className="menu-component">Ipad</div>
+            <div className="menu-component">Watch</div>
+            <div className="menu-component">Âm thanh</div>
+            <div className="menu-component">Phụ kiện</div>
+          </div>
+          <div className="nav-input">
+            <input type="text" className="products-input" placeholder='Tìm kiếm sản phẩm'/>
+          </div>
+          <div className="shopping-icon">
+            <img src={shopping_icon} alt="" className='icon'/>
+          </div>
+          <div className="user-icon">
+            <img src={user_icon} alt="" className='icon'/>
+          </div>
+        </div>
+        </div>
     </div>
   )
 }
