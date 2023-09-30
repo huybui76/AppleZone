51f1da9a9091a63dbb8cdf6cc7ab2bfd30f66cbd
Diệp Phi <127822124+Jellyvous@users.noreply.github.com>
2023-09-30 22:11:41 +0700

    cap nhat menu sales

--- src/components/pages/page components/ProductItem/ProductItem.jsx
+++ src/components/pages/page components/ProductItem/ProductItem.jsx
@@ -5,12 +5,15 @@ function ProductItem({ product }) {
   return (
     <div className='product-container'>
       <div className="img-container">
-        <div className="sales-percentage">-40%</div>
+        <div className="sales-percentage">{product.sales_percentage}</div>
+        <div className="product-img">
+          <img src={product.image} alt={product.name} />
+        </div>
       </div>
-      <div className='product-name'>{product.name}</div>
+      <div className='product-name'>{product.name} </div>
       <div className="product-price">
-        <div className="sale-price">1.800.000 Đ</div>
-        <div className="original-price">2.000.000 Đ</div>
+        <div className="sales-price">{product.sales_price} Đ</div>
+        <div className="original-price">{product.original_price} Đ</div>
       </div>
 
     </div>
